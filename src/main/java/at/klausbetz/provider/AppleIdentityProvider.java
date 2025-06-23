fc8dfac6355a68f597abe22b725f356c1ce71180
Yu-Hsiang Huang <yhhuang79@gmail.com>
2025-06-23 15:46:18 +0800

    Add warn logger

--- src/main/java/at/klausbetz/provider/AppleIdentityProvider.java
+++ src/main/java/at/klausbetz/provider/AppleIdentityProvider.java
@@ -243,7 +243,7 @@ public class AppleIdentityProvider extends OIDCIdentityProvider implements Socia
         jwt.audience(ISSUER);
         jwt.iat((long) Time.currentTime());
         jwt.exp(jwt.getIat() + 86400 * 180);
-        logger.debug("generateClientToken=" + jwt);
+        logger.warn("generateClientToken=" + jwt);
         return jwt;
     }
 
